36ef28532e4bcc87baaa1a6bdc956f90638cfd4d
Sinemp <103983721+Sinemp@users.noreply.github.com>
2025-06-07 01:26:26 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "sub"         # 用户名
+UUID = "30f535bc-a08a-4b21-b492-5b5f3852a0cd"                     # UUID，留空则自动生成
+PORT = 31800                   # Vmess端口，留空或0则自动生成
+DOMAIN = "streamlit.snake.pp.ua"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiM2VmOTRkODViNGQzZmQ5OTc2Yzc5MDFmZmUyYWZkNTUiLCJ0IjoiZjNlZjRlODAtMWY0NS00OGMxLWE3NTEtNzc4ZDA0NjE5ODIyIiwicyI6Ik0yVm1NRFV6TkRJdE1EVXlNUzAwWkRSaExXSTNOV1F0TnpneU9EVTVPV1ptTnpjNCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
